aec6d94c045385d69ad8d3930c2a6722a03dba9d
Kevin Earls <kevinearls@mac.com>
2023-12-31 13:49:19 +0100

    Initial Commit
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by Kevin Earls on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
